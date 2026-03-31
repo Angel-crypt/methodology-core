3b35b115878aaec51de359a87abca99d5d83c849
Angel-crypt <angel.cruz.030905@gmail.com>
2026-03-30 22:23:49 -0600

    feat: add initial implementation of instruments module with repository, service, schemas, and router

---
+++ backend/app/api/v1/instruments/schemas.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+
+from pydantic import BaseModel
+
+
+class InstrumentCreateRequest(BaseModel):
+    # TODO: campos derivados del contrato XML
+    pass
+
+
+class InstrumentUpdateRequest(BaseModel):
+    # TODO: campos derivados del contrato XML
+    pass
+
+
+class InstrumentResponse(BaseModel):
+    # TODO: campos derivados del contrato XML
+    pass
