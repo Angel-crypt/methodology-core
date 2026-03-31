33aa36975cb0104b16bc3b9701815e1444d57df2
Angel-crypt <angel.cruz.030905@gmail.com>
2026-03-30 22:24:04 -0600

    feat: add initial implementation of metrics module with repository, service, schemas, router, and dependencies

---
+++ backend/app/api/v1/metrics/schemas.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+
+from pydantic import BaseModel
+
+
+class MetricCreateRequest(BaseModel):
+    # TODO: campos derivados del contrato XML
+    pass
+
+
+class MetricUpdateRequest(BaseModel):
+    # TODO: campos derivados del contrato XML
+    pass
+
+
+class MetricResponse(BaseModel):
+    # TODO: campos derivados del contrato XML
+    pass
